5bf16bcdadfac13a93bddc51b8aa873f5779a6ec
atlas <atlas@Rejoices-MacBook-Air.local>
2018-04-13 08:48:16 -0700

    Convert ListContacts to a Stateless Functional Component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,29 +1,36 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class ListContacts extends Component {
-      render () {
+function ListContacts (props){
+  return(
+    <ol className="contact-list">
+      {props.contacts.map((contact) => (
+        <li key={contact.id} className='contact-list-item' >
+          <div className='contact-avatar'
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}></div>
+          <div className = 'contact-details'>
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <button className='contact-remove'>
+            remove
+          </button>
+        </li>
+      ))}
+    </ol>
 
-          return(
-            <ol className="contact-list">
-              {this.props.contacts.map((contact) => (
-                <li key={contact.id} className='contact-list-item' >
-                  <div className='contact-avatar'
-                    style={{
-                      backgroundImage: `url(${contact.avatarURL})`
-                    }}></div>
-                  <div className = 'contact-details'>
-                    <p>{contact.name}</p>
-                    <p>{contact.handle}</p>
-                  </div>
-                  <button className='contact-remove'>
-                    remove
-                  </button>
-                </li>
-              ))}
-
-            </ol>
-          )
-      }
+  )
 }
+
+
+// class ListContacts extends Component {
+//       render () {
+//
+//           // return(
+//           //
+//           // )
+//       }
+// }
 
 export default ListContacts
