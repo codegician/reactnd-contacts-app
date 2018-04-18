7b37af7c92f2f81f2e18a8b4fe948ee2722e453a
El Jackson <hussledna@icloud.com>
2018-04-17 17:42:04 -0700

    save new contact to server

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ import CreateContact from './CreateContact'
 import { Route } from 'react-router-dom'
 
 class App extends Component {
-  state ={
-    contacts: [ ],
+  state = {
+    contacts: []
   }
-
   componentDidMount() {
     ContactsAPI.getAll()
-    .then((contacts) => {
-      this.setState(() => ({
-        contacts
-      }))
-    })
-
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
   }
-
   removeContact = (contact) => {
     this.setState((currentState) => ({
       contacts: currentState.contacts.filter((c) => {
@@ -26,21 +23,36 @@ class App extends Component {
       })
     }))
 
-  ContactsAPI.remove(contact)
+    ContactsAPI.remove(contact)
+  }
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
   }
   render() {
     return (
       <div>
         <Route exact path='/' render={() => (
           <ListContacts
-           contacts={this.state.contacts}
-           onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
           />
-          )} />
-        <Route path='/create' component={CreateContact}/>
+        )} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
       </div>
     )
   }
 }
 
-export default App;
+export default App
